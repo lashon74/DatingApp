c3c7d680f1293045b3e99466e81cb86fdc56181e
Lashon Hudson <lashonhud@gmail.com>
2025-04-22 13:47:22 -0400

    adding login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +29,31 @@ namespace API.Controllers
              await context.SaveChangesAsync();
 
              return user;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<AppUser>>  Login(LoginDto loginDto)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+            if(user == null) 
+            {
+                return Unauthorized("Invalid username");
+            } 
+             
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash =  hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid password");
+            }
+            }
+           
+                return user;
         }
 
         //checking to see if a duplicate username is being used
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574ed16e7d9705658ca615aca7e902b89e0572c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f116ac8a43b43c74b3886fae7f2ec5a4200b5e77")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
