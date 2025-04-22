61fd0dedc0597aea63e2d534c70a6861bec79100
Lashon Hudson <lashonhud@gmail.com>
2025-04-22 12:17:02 -0400

    using DTO data transfer object

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,20 +3,24 @@ using System.Text;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AccountController(DataContext context) : BaseApiController
     {
         [HttpPost("register")] //account/register
-        public async Task<ActionResult<AppUser>> Register([]string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is already taken!");
+
             //garbage collector will dispose of this when it is not being used 
             using var hmac = new HMACSHA512();
              var user = new AppUser
              {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
              };
 
@@ -24,6 +28,11 @@ namespace API.Controllers
              await context.SaveChangesAsync();
 
              return user;
+        }
+
+        //checking to see if a duplicate username is being used
+        private async Task<bool> UserExists(string username){
+            return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
         }
     }
 }
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7265e4142b120753a36c673c10cde19fc0272936")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574ed16e7d9705658ca615aca7e902b89e0572c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
