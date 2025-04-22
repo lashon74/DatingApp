97273641113bdd6850c0e4c176a12c9453679791
Lashon Hudson <lashonhud@gmail.com>
2025-04-21 20:29:49 -0400

    adding bootstrap

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acd4b51bea08a4f5f24177e67757eb37d118723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+417ffb52f2dfe27a9221e154afd211c551301999")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
