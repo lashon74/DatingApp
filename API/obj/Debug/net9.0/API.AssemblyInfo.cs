7424d82d890d31cd91be1474d4a7b7a094d714c8
Lashon Hudson <lashonhud@gmail.com>
2025-04-23 12:16:17 -0400

    Adding home page

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8ca34f44fcb0718d786574bd991c06738c2368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a550734e58039dc1742ff55424760c3c0063ffef")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
