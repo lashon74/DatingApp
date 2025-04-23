52eb88a6a3ce562efcef1f05ab0976880b7df72d
Lashon Hudson <lashonhud@gmail.com>
2025-04-23 09:26:51 -0400

    angular services using them to inject in the app similar to DI in .NET

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793f19bb544c73d2b8ae250d40a93459cb753710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba2c2547bb86074ae02ca038b31595f958b641df")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
